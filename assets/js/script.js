30dc5a4bc87ef0dc67b7f0f414ae905fa7bce13a
jakelw96 <jake.lw96@gmail.com>
2021-04-18 00:30:07 -0600

    Working on logic for saving text in description box. Created a function create P element

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,36 +129,50 @@ var currentTime = function() {
 };
 currentTime();
 
+//Function to create a new p element when the div is clicked
+//var createText = function() {/
+  //var text = nineBlock.text()
+  //var newText = $("<p>");
+  //$(this).attr("contentEditable", true);
+  //$(newText).text(text);
+//}
+
 // To create element that makes up the description text
 $("#nine").click(function() {
-  // checks curent text of descriptions
-  var currentText = $(this)
-    .text()
-    .trim();
-
-  // replace element with new textarea
-  var input = $("<textarea>").addClass("description").val(currentText);
-  $(this).replaceWith(input);
-  
+  if(!$.trim($("#nine").html()).length) {
+    $(this).attr("contentEditable", true); 
+  } else {
+    $(this).attr("contentEditable", false);
+  }
 });
-
-// To load input
-var loadInput = function() {
-  input = JSON.parse(localStorage.getItem("input"));
-};
-
-// To save input in description to localStorage
-var saveInput = function() {
-  localStorage.setItem("input", JSON.stringify(input));
-};
 
 // saves input to localStorage and appends to the div
 $("#nineBtn").click(function() {
-  var input = $("#nine").value
-  
-  $("#nine").append(input);
+  $("#nine").attr("contentEditable", false);
+  var input = $("#nine").html();
+  var textP = $("<p>");
+  $(textP).addClass("textColor");
+  $(textP).attr("id", "textP");
+  textP.value = input;
+  var newText = textP.value;
+  $("#nine").empty();
+  $(textP).append(newText);
+  $("#nine").append(textP);
 });
 
+// Allows to update text in div
+$("#nine").on("click", "p", function() {
+  var text = $(this)
+  .text()
+  .trim();
 
+  console.log("p was clicked");
+  
+  $("#nineBtn").click(function() {
+      console.log("save btn was clicked with an element already on the page");
+  })
+})
+    
 
+// create if statement to only execute creating a p element if there isn't one currently in the div
 
